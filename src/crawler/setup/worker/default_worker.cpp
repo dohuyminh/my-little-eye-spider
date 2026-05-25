2b39dede000dd72b41df33e364bca05b9c6f8803
dohuyminh <minhdohuy2111@gmail.com>
2026-05-25 10:24:55 +1000

    [FRONTIER] set up worker flow

--- src/crawler/setup/worker/default_worker.cpp
+++ src/crawler/setup/worker/default_worker.cpp
@@ -1,8 +1,11 @@
 #include "default_worker.h"
+
 #include <string>
 
+#include "rewards.h"
 #include "services/html_downloader.h"
 #include "services/html_parser.h"
+#include "types/url.h"
 
 namespace crawler::setup {
 
@@ -25,7 +28,18 @@ void DefaultWorker::doWork() {
     for (const auto& nextUrl : result.outlinks()) {
       // process outlinks and insert back to frontier
       // check if nextUrl is a relative URL
+      types::URL baseURL {data_.url()};
+      if (baseURL.applyPath(nextUrl)) {
+        producingURLs_->try_enqueue(baseURL.to_string());
+      } 
+      else {
+        producingURLs_->try_enqueue(nextUrl);
+      }
     }
+
+    double r {reward(data_.url())};
+    data_.setNormalizedReward(r);
+    frontier_.sendUpdate(data_.updateCtx());
 
   } catch (std::invalid_argument const& e) {
     return;
