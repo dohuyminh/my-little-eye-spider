1ee33dc63fa88e4e7ab98369cd181982fcb2db08
dohuyminh <minhdohuy2111@gmail.com>
2026-07-03 12:43:10 +1000

    [DATABASE] WIP

--- src/main.cpp
+++ src/main.cpp
@@ -10,57 +10,66 @@
 #include "crawler/services/html_parser.h"
 #include "crawler/types/url.h"
 
+#include "dotenv.h"
+
 using namespace crawler::services::html;
 using namespace crawler::services::curl;
 
+using namespace dotenv;
+
 int main() {
-  HTMLParser<LexborParser> parser;
-  std::string url;
-  do {
-    std::cout << "Insert a valid URL here:\n> ";
+  // HTMLParser<LexborParser> parser;
+  // std::string url;
+  // do {
+  //   std::cout << "Insert a valid URL here:\n> ";
+  //
+  //   bool valid{false};
+  //   while (!valid) {
+  //     std::getline(std::cin, url);
+  //     try {
+  //       if (url != "x") {
+  //         crawler::types::URL{url};
+  //       }
+  //       valid = true;
+  //     } catch (std::invalid_argument const&) {
+  //       std::cerr << std::format(
+  //           "\"{}\" is not a valid URL; please try again\n", url);
+  //       std::cout << "> ";
+  //     }
+  //   }
+  //
+  //   if (url == "x") {
+  //     break;
+  //   }
+  //
+  //   ParseResult result;
+  //   try {
+  //     auto oURL{crawler::types::URL{url}};
+  //     std::cout << oURL.domain() << '\n';
+  //     Response resp{Downloader()(oURL)};
+  //     if (resp.code() != 200) {
+  //       std::cerr << std::format("[HTTP] GET method returned code {}\n",
+  //                                resp.code());
+  //       continue;
+  //     }
+  //     result = parser.parse(resp.content());
+  //   } catch (std::runtime_error const& e) {
+  //     std::cerr << std::format(
+  //         "[HTTP] cannot perform GET request on URL \"{}\"\n", url);
+  //     std::cerr << e.what() << '\n';
+  //     continue;
+  //   }
+  //
+  //   for (const auto& outlink : result.outlinks()) {
+  //     std::cout << std::format("[Outlink] {}\n", outlink);
+  //   }
+  //
+  // } while (url != "x");
+  //
+  
+  env.load_dotenv("../.env", false, true);
 
-    bool valid{false};
-    while (!valid) {
-      std::getline(std::cin, url);
-      try {
-        if (url != "x") {
-          crawler::types::URL{url};
-        }
-        valid = true;
-      } catch (std::invalid_argument const&) {
-        std::cerr << std::format(
-            "\"{}\" is not a valid URL; please try again\n", url);
-        std::cout << "> ";
-      }
-    }
-
-    if (url == "x") {
-      break;
-    }
-
-    ParseResult result;
-    try {
-      auto oURL{crawler::types::URL{url}};
-      std::cout << oURL.domain() << '\n';
-      Response resp{Downloader()(oURL)};
-      if (resp.code() != 200) {
-        std::cerr << std::format("[HTTP] GET method returned code {}\n",
-                                 resp.code());
-        continue;
-      }
-      result = parser.parse(resp.content());
-    } catch (std::runtime_error const& e) {
-      std::cerr << std::format(
-          "[HTTP] cannot perform GET request on URL \"{}\"\n", url);
-      std::cerr << e.what() << '\n';
-      continue;
-    }
-
-    for (const auto& outlink : result.outlinks()) {
-      std::cout << std::format("[Outlink] {}\n", outlink);
-    }
-
-  } while (url != "x");
+  std::cout << env["POSTGRESQL_DB_HOST"] << '\n';
 
   std::cout << "Exiting Program...\n";
 
